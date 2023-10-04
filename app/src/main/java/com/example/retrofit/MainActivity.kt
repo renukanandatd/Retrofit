0268b5b84936e73d5fbba8f8c20714d037835cc8
renukanandatd <renukananda2000@gmail.com>
2023-10-04 14:32:13 +0530

    Retrived data using API

--- app/src/main/java/com/example/retrofit/MainActivity.kt
+++ app/src/main/java/com/example/retrofit/MainActivity.kt
@@ -2,6 +2,12 @@ package com.example.retrofit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.Toast
+import com.example.retrofit.databinding.ActivityMainBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.create
@@ -9,9 +15,16 @@ import retrofit2.create
 class MainActivity : AppCompatActivity() {
 
     private val baseURL = "https://jsonplaceholder.typicode.com/"
+
+    lateinit var mainBinding: ActivityMainBinding
+    var postList = ArrayList<Posts>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        val view = mainBinding.root
+        setContentView(view)
+
+        showPosts()
     }
 
     fun showPosts(){
@@ -21,5 +34,32 @@ class MainActivity : AppCompatActivity() {
             .addConverterFactory(GsonConverterFactory.create())
             .build()
         val retrofitAPI : RetrofitAPI = retrofit.create(RetrofitAPI::class.java)
+        val call : Call<List<Posts>> = retrofitAPI.getAllPosts()
+
+        call.enqueue(object : Callback<List<Posts>>{
+            override fun onResponse(call: Call<List<Posts>>, response: Response<List<Posts>>) {
+
+                if(!response.isSuccessful){
+                    mainBinding.textViewUseId.text = "ERROR!!!"
+                    mainBinding.textViewId.text = "ERROR!!!"
+                    mainBinding.textViewTitle.text = "ERROR!!!"
+                    mainBinding.textViewBody.text = "ERROR!!!"
+                }
+
+                postList = response.body() as ArrayList<Posts>
+
+                mainBinding.textViewUseId.text = postList[0].userId.toString()
+                mainBinding.textViewId.text = postList[0].id.toString()
+                mainBinding.textViewTitle.text = postList[0].title
+                mainBinding.textViewBody.text = postList[0].subTitle
+
+            }
+
+            override fun onFailure(call: Call<List<Posts>>, t: Throwable) {
+                Toast.makeText(applicationContext,t.localizedMessage,Toast.LENGTH_LONG).show()
+            }
+
+
+        })
     }
 }
