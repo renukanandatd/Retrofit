5cda79d18409708b2337d7bca769e99f795b6c1d
renukanandatd <renukananda2000@gmail.com>
2023-10-04 13:37:18 +0530

    Initial commit

---
+++ app/src/main/java/com/example/retrofit/Posts.kt
@@ -0,0 +1,12 @@
+package com.example.retrofit
+
+import com.google.gson.annotations.SerializedName
+
+data class Posts(
+    val userId : Int,
+    val id : Int,
+    val title : String,
+    @SerializedName("body")
+    val subTitle : String
+) {
+}
